1a84fa49c77f7888724d652755a8b56c4d2dabaf
Alberto Scala <31989626+albertoscala@users.noreply.github.com>
2023-12-12 11:57:03 +0100

    dynamic dashboard created

--- c2-gui.py
+++ c2-gui.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import requests
 
 HOST = "localhost"
 PORT_HTTP_CLIENT = 8080
@@ -12,9 +13,26 @@ class MyRequestHandler(BaseHTTPRequestHandler):
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
 
-                # read the html file
-                with open("index.html", "r") as file:
-                    html = file.read()
+                # read the top html file
+                with open("hp-top.html", "r") as f:
+                    tp_html = f.read()
+
+                # read the bottom html file
+                with open("hp-bttm.html", "r") as f:
+                    bt_html = f.read()
+                
+                # make get http request to the server to get the clients list
+                clients = requests.get("http://localhost:8880/clients").json()
+
+                print(clients)
+
+                # create the html elements for the clients list
+                clients_html = ""
+                for client in clients:
+                    clients_html += f'<li class="list-group-item"><a href="#">{client}</a></li>'
+
+                # join all the html file parts
+                html = tp_html + clients_html + bt_html
 
                 # send the html file to the client
                 self.wfile.write(html.encode())
@@ -22,7 +40,7 @@ class MyRequestHandler(BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     # Create an HTTP server instance
-    server = HTTPServer(('localhost', 8000), MyRequestHandler)
+    server = HTTPServer((HOST, PORT_HTTP_CLIENT), MyRequestHandler)
 
     # Start the server
     server.serve_forever()    
