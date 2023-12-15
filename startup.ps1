b6c6255550515a099ae667194c523284e30f48c8
Alberto S <alberto.scala.privata@gmail.com>
2023-12-15 10:05:49 +0100

    added startup script

---
+++ startup.ps1
@@ -0,0 +1,5 @@
+# Start c2.py in a new shell
+Start-Process -FilePath "python" -ArgumentList "c2.py" -NoNewWindow
+
+# Start c2-gui.py in a new shell
+Start-Process -FilePath "python" -ArgumentList "c2-gui.py" -NoNewWindow
